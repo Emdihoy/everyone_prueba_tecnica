315659b43d6dfcb831e832e89b8ac4231e08a274
Emilio <136965828+Emdihoy@users.noreply.github.com>
2024-04-05 12:13:02 +0000

    DB Model created

--- src/api/models.py
+++ src/api/models.py
@@ -2,11 +2,18 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    nombre = db.Column(db.String(120), unique=True, nullable=False)
+    correo = db.Column(db.String(120), unique=True, nullable=False)
+    fecha_de_registro = db.Column(db.String(120), unique=False, nullable=False)
+    pregunta1 = db.Column(db.String(120), unique=False, nullable=False)
+    respuesta1 = db.Column(db.String(120), unique=False, nullable=False)
+    pregunta2 = db.Column(db.String(120), unique=False, nullable=False)
+    respuesta2 = db.Column(db.String(120), unique=False, nullable=False)
+    pregunta3 = db.Column(db.String(120), unique=False, nullable=False)
+    respuesta3 = db.Column(db.String(120), unique=False, nullable=False)
+    fecha_de_questionario = db.Column(db.String(120), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +22,5 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "fecha_de_registro": self.fecha_de_registro
         }
